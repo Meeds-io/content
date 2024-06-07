f6210c2ef2bb813ba49ff95c2c30eaaa36e88d23
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-06-07 12:02:17 +0100

    feat: Add translation option in news list view header title setting - EXO-65563 - Meeds-io/MIPs#128 (#98)

--- content-webapp/src/main/webapp/vue-app/news-list-view/main.js
+++ content-webapp/src/main/webapp/vue-app/news-list-view/main.js
@@ -60,11 +60,11 @@ const url = `/content/i18n/locale.portlet.news.News?lang=${lang}`;
 
 // getting locale resources
 export function init(params) {
+  const applicationId = params.applicationId;
   const appId = params.appId;
   const viewTemplate = params.viewTemplate;
   const saveSettingsURL = params.saveSettingsURL;
   const newsTarget = params.newsTarget;
-  const header = params.header;
   const limit = params.limit === '' ? '4' : params.limit;
   const showHeader = viewTemplate === 'NewsSlider' ? false: params.showHeader === 'true';
   const showSeeAll = params.showSeeAll === 'true' && !!params.seeAllUrl?.length;
@@ -81,10 +81,12 @@ export function init(params) {
     // init Vue app when locale resources are ready
     Vue.createApp({
       data: {
+        applicationId,
+        headerTranslations: null,
         saveSettingsURL,
         viewTemplate,
         newsTarget,
-        header,
+        headerTitle: null,
         limit,
         showHeader,
         showSeeAll,
@@ -97,11 +99,29 @@ export function init(params) {
         showArticleDate,
         seeAllUrl
       },
+      computed: {
+        newListTranslationEnabled() {
+          return eXo?.env?.portal?.newsListTranslationEnabled;
+        },
+      },
+      created() {
+        if (!this.newListTranslationEnabled) {
+          this.headerTitle = params.headerTitle;
+          return;
+        }
+        Vue.prototype.$translationService.getTranslations('newsListView', applicationId, 'headerNameInput').then(translations => {
+          this.headerTranslations = translations;
+          this.headerTitle = translations?.[lang] || translations?.en
+                                                  || params.headerTitle;
+        });
+      },
       template: `<news-list-view
                   id="${appId}"
+                  :application-id="applicationId"
+                  :header-translations="headerTranslations"
                   :view-template="viewTemplate"
                   :news-target="newsTarget"
-                  :header="header"
+                  :header-title="headerTitle"
                   :show-article-author="showArticleAuthor"
                   :show-article-image="showArticleImage"
                   :show-article-reactions="showArticleReactions"
