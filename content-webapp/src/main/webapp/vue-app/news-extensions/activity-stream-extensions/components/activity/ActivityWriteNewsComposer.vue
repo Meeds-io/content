a0a3100ff05190154a8b458755482236ced964d3
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2024-07-10 18:34:09 +0100

    feat: Enable to mention user/group from content editor - EXO-72644 - Meeds-io/MIPs#128 (#174)

--- content-webapp/src/main/webapp/vue-app/news-extensions/activity-stream-extensions/components/activity/ActivityWriteNewsComposer.vue
+++ content-webapp/src/main/webapp/vue-app/news-extensions/activity-stream-extensions/components/activity/ActivityWriteNewsComposer.vue
@@ -92,7 +92,7 @@ export default {
       if (!this.disableComposerButton) {
         let url = `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/news/editor`;
         if (eXo.env.portal.spaceId) {
-          url += `?spaceId=${eXo.env.portal.spaceId}&type=draft`;
+          url += `?spaceId=${eXo.env.portal.spaceId}&spaceName=${eXo.env.portal.spaceName}&type=draft`;
         }
         localStorage.setItem('exo-activity-composer-message', this.message || '');
         window.open(url, '_blank');
