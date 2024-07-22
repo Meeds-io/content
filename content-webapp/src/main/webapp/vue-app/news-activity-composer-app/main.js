5710415a81efdddd5a6840a889444eacf76a9b10
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2024-07-22 11:04:21 +0100

    feat: include select image extension in the content rich editor - EXO-73170 - meeds-io/MIPs#128 (#185)

--- content-webapp/src/main/webapp/vue-app/news-activity-composer-app/main.js
+++ content-webapp/src/main/webapp/vue-app/news-activity-composer-app/main.js
@@ -83,7 +83,7 @@ export function init(maxToUpload, maxFileSize) {
       i18n,
       vuetify
     });
-  });
+  }).finally(() => Vue.prototype.$utils.includeExtensions('WYSIWYGPluginsExtensions'));
 }
 
 export function destroy() {
