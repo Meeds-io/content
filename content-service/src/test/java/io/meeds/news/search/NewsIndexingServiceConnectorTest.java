38cc385b42fd71adf272d7b9c51626d7f9481cd6
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2024-08-02 16:46:27 +0100

    feat: Fix NewsIndexingServiceConnector initialization - EXO-73544 - Meeds-io/MIPs#128 (#201)
    Prior to this change, after updating the NewsIndexingServiceConnector as
    a Spring component, this connector was added to the
    IndexingOperationProcessor after its initialization, resulting in the
    news alias not being created. This change will update the way XML data
    initialization is used, resolving this issue.

--- content-service/src/test/java/io/meeds/news/search/NewsIndexingServiceConnectorTest.java
+++ content-service/src/test/java/io/meeds/news/search/NewsIndexingServiceConnectorTest.java
@@ -22,17 +22,12 @@ package io.meeds.news.search;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
 
 import java.util.Date;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -55,6 +50,8 @@ import io.meeds.news.service.NewsService;
 @RunWith(MockitoJUnitRunner.class)
 public class NewsIndexingServiceConnectorTest {
 
+  NewsIndexingServiceConnector newsIndexingServiceConnector = null;
+
   @Mock
   NewsService                  newsService;
 
@@ -67,16 +64,13 @@ public class NewsIndexingServiceConnectorTest {
   @Mock
   MetadataService              metadataService;
 
-  @InjectMocks
-  NewsIndexingServiceConnector newsIndexingServiceConnector;
-
-  @Before
-  public void setUp() {
-    openMocks(this);
-  }
-
   @Test
   public void testGetAllIds() {
+    newsIndexingServiceConnector = new NewsIndexingServiceConnector(identityManager,
+                                                                    getParams(),
+                                                                    newsService,
+                                                                    activityManager,
+                                                                    metadataService);
     try {
       newsIndexingServiceConnector.getAllIds(0, 10);
       fail("getAllIds shouldn't be supported");
@@ -87,6 +81,12 @@ public class NewsIndexingServiceConnectorTest {
 
   @Test
   public void testCreate() {
+    newsIndexingServiceConnector = new NewsIndexingServiceConnector(identityManager,
+                                                                    getParams(),
+                                                                    newsService,
+                                                                    activityManager,
+                                                                    metadataService);
+
     try {
       newsIndexingServiceConnector.create(null);
       fail("IllegalArgumentException should be thrown");
@@ -129,7 +129,7 @@ public class NewsIndexingServiceConnectorTest {
     posterProfile.setProperty("fullName", "Root Root");
     posterIdentity.setProfile(posterProfile);
     try {
-      when(newsService.getNewsArticleById(anyString())).thenReturn(news);
+      when(newsService.getNewsArticleById("1")).thenReturn(news);
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -148,4 +148,14 @@ public class NewsIndexingServiceConnectorTest {
     assertNotNull(document.getPermissions());
     assertEquals(1, document.getPermissions().size());
   }
+
+  private InitParams getParams() {
+    InitParams params = new InitParams();
+    PropertiesParam propertiesParam = new PropertiesParam();
+    propertiesParam.setName("constructor.params");
+    params.addParameter(propertiesParam);
+    propertiesParam.setProperty("index_current", "index_name");
+    return params;
+  }
+
 }
