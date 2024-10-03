67fa56b5fafd724b9f627e5f02bd43bb43926501
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-10-03 12:37:55 +0100

    feat: Implement new article view layout - EXO-73043 - Meeds-io/MIPs#129 (#244)
    Implement new article view layout

--- content-webapp/src/main/webapp/vue-app/news/components/NewsMobileActionMenu.vue
+++ content-webapp/src/main/webapp/vue-app/news/components/NewsMobileActionMenu.vue
@@ -33,67 +33,66 @@
         :show-publish-button="showPublishButton"
         :show-resume-button="showResumeButton"
         :show-share-button="showShareButton"
-        @copy-link="$emit('copy-link')"
+        @copy-link="copyLink"
         @edit-article="$emit('edit-article', news)"
-        @delete-article="$emit('delete-article')" />
+        @delete-article="$emit('delete-article', news)" />
     </template>
   </exo-drawer>
 </template>
 
 <script>
 export default {
-  props: {
-    news: {
-      type: Object,
-      required: false,
-      default: null
-    },
-    showShareButton: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    showEditButton: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    showResumeButton: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    showDeleteButton: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    showPublishButton: {
-      type: Boolean,
-      required: false,
-      default: false
-    },
-    currentApp: {
-      type: String,
-      required: false,
-      default: null
-    },
-    showCopyLinkButton: {
-      type: Boolean,
-      required: false,
-      default: false
-    }
+  data() {
+    return {
+      news: null,
+      showShareButton: false,
+      showEditButton: false,
+      showResumeButton: false,
+      showDeleteButton: false,
+      showPublishButton: false,
+      currentApp: null,
+      showCopyLinkButton: false
+    };
   },
   created() {
     this.$root.$on('open-news-mobile-action-menu', this.open);
     this.$root.$on('close-news-mobile-action-menu',this.close);
   },
   methods: {
-    open() {
+    open(config) {
+      this.news = config.news;
+      this.showShareButton = config.showShareButton;
+      this.showEditButton = config.showEditButton;
+      this.showResumeButton = config.showResumeButton;
+      this.showDeleteButton = config.showDeleteButton;
+      this.showPublishButton = config.showCopyLinkButton;
+      this.showCopyLinkButton = config.showCopyLinkButton;
+      this.currentApp = config.currentApp;
       this.$refs.newsMobileActionMenu.open();
     },
     close() {
       this.$refs.newsMobileActionMenu.close();
+    },
+    copyLink() {
+      const portalName = eXo.env.portal.metaPortalName;
+      const baseUrl = window.location.href.split(portalName)[0] + portalName;
+
+      const newsLink = this.news.published && this.news.audience === 'all'
+        ? `${baseUrl}/news-detail?newsId=${this.news.id}&type=article`
+        : `${baseUrl}/activity?id=${this.news.activityId}`;
+
+      const finalLink = this.news.lang ? `${newsLink}&lang=${this.news.lang}` : newsLink;
+
+      navigator.clipboard.writeText(finalLink)
+        .then(() => {
+          document.dispatchEvent(new CustomEvent('alert-message', {
+            detail: {
+              alertType: 'success',
+              alertMessage: this.$t('news.alert.success.label.linkCopied'),
+            }
+          }));
+          this.close();
+        });
     }
   }
 };
