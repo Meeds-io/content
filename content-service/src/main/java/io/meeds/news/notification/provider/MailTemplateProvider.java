f8e01e24b2bb6ffba179a7c675ee04eb70b3a53a
azayati <azayati@exoplatform.com>
2024-10-07 13:29:18 +0200

    fix: Send published article public link in publish news mail notification message - EXO-73050 - Meeds-io/MIPs#129

--- content-service/src/main/java/io/meeds/news/notification/provider/MailTemplateProvider.java
+++ content-service/src/main/java/io/meeds/news/notification/provider/MailTemplateProvider.java
@@ -111,8 +111,7 @@ public class MailTemplateProvider extends TemplateProvider {
       templateContext.put("AUTHOR_AVATAR_URL", encoder.encode(authorAvatarUrl));
       templateContext.put("CONTEXT", encoder.encode(context));
       StringBuilder activityUrl = new StringBuilder();
-      Space space = spaceService.getSpaceByDisplayName(contentSpaceName);
-      if (pluginId.equals(PublishNewsNotificationPlugin.ID) && !spaceService.isMember(space, notification.getTo())) {
+      if (pluginId.equals(PublishNewsNotificationPlugin.ID)) {
         String portalName = PortalContainer.getCurrentPortalContainerName();
         String portalOwner = CommonsUtils.getCurrentPortalOwner();
         String currentDomain = CommonsUtils.getCurrentDomain();
@@ -125,8 +124,6 @@ public class MailTemplateProvider extends TemplateProvider {
                    .append(portalOwner)
                    .append("/news-detail?newsId=")
                    .append(newsId);
-      } else {
-        activityUrl.append(activityLink);
       }
 
       templateContext.put("ACTIVITY_LINK", encoder.encode(activityUrl.toString()));
