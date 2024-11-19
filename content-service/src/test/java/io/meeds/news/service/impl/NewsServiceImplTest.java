c206ebd7e7d5cf35330dcdc47105de2c0c42898e
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-11-19 19:30:19 +0100

    fix: fix publish article from empty note default home page - EXO-73041 - Meeds-io/MIPs#161 (#313)
    prior to this change, when try to publish newly created default note
    home page, an exception is thrown as the note is auto created without
    specifying the author.
    This PR ensures to set the poster as author during the publish process.

--- content-service/src/test/java/io/meeds/news/service/impl/NewsServiceImplTest.java
+++ content-service/src/test/java/io/meeds/news/service/impl/NewsServiceImplTest.java
@@ -564,6 +564,11 @@ public class NewsServiceImplTest {
             anyMap(),
             anyLong(),
             anyBoolean());
+    
+    clearInvocations(activityManager);
+    newsArticlePage.setAuthor(null);
+    newsService.createNews(newsArticle, identity);
+    verify(activityManager, times(1)).saveActivityNoReturn(any(), any());
   }
 
   @Test
