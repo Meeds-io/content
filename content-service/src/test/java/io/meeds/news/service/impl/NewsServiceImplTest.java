0e4a11e8c232454d0dff362bb31928501dbabfa2
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-11-19 10:29:20 +0100

    feat: Implement edit schedule of article created from exiting note page - EXO-75389 - Meeds-io/MIPs#161 (#311)
    Implement edit schedule of article created from exiting note page
    
    fix: fix publish article from empty note default home page - EXO-73041 - Meeds-io/MIPs#161 (#313)
    
    prior to this change, when try to publish newly created default note
    home page, an exception is thrown as the note is auto created without
    specifying the author.
    This PR ensures to set the poster as author during the publish process.

--- content-service/src/test/java/io/meeds/news/service/impl/NewsServiceImplTest.java
+++ content-service/src/test/java/io/meeds/news/service/impl/NewsServiceImplTest.java
@@ -563,6 +563,11 @@ public class NewsServiceImplTest {
             anyMap(),
             anyLong(),
             anyBoolean());
+    
+    clearInvocations(activityManager);
+    newsArticlePage.setAuthor(null);
+    newsService.createNews(newsArticle, identity);
+    verify(activityManager, times(1)).saveActivityNoReturn(any(), any());
   }
 
   @Test
@@ -950,6 +955,7 @@ public class NewsServiceImplTest {
 
     mockBuildArticle(metadataItems);
     Space space = mockSpace();
+    when(space.getGroupId()).thenReturn("/spaces/test");
     Wiki wiki = mock(Wiki.class);
     when(wikiService.getWikiByTypeAndOwner(anyString(), anyString())).thenReturn(wiki);
     org.exoplatform.wiki.model.Page rootPage = mock(org.exoplatform.wiki.model.Page.class);
@@ -976,10 +982,17 @@ public class NewsServiceImplTest {
     when(identityManager.getOrCreateUserIdentity(anyString())).thenReturn(identity1);
     when(identity1.getId()).thenReturn("1");
 
-    newsService.unScheduleNews(newsArticle, space.getGroupId(), "john");
+    newsService.unScheduleNews(newsArticle, space, "john");
 
     verify(noteService, times(1)).createDraftForNewPage(any(DraftPage.class), anyLong(), anyLong());
     verify(noteService, times(1)).deleteNote(anyString(), anyString(), anyString());
+
+    clearInvocations(noteService);
+    properties.put(EXTERNAL_PAGE, "true");
+    newsService.unScheduleNews(newsArticle, space, "john");
+    verify(noteService, times(0)).createDraftForNewPage(any(DraftPage.class), anyLong(), anyLong());
+    verify(noteService, times(0)).deleteNote(anyString(), anyString(), anyString());
+    verify(metadataService, times(2)).deleteMetadataItemsByObject(any());
   }
 
   @Test
