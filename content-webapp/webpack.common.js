7f5af14f375963b6f6538d3aa3187efe61f35d68
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-12-19 10:15:26 +0100

    feat: Remove MIP-161 feature flags - EXO-76014 - Meeds-io/MIPs#161 (#348)
    Remove MIP-161 feature flags

--- content-webapp/webpack.common.js
+++ content-webapp/webpack.common.js
@@ -7,14 +7,12 @@ let config = {
   // set the entry point of the application
   // can use multiple entry
   entry: {
-    newsTargetSelectorComponent :'./src/main/webapp/vue-app/targetSelector/main.js',
     newsActivityComposer :'./src/main/webapp/vue-app/news-activity-composer-app/main.js',
     newsDetails :'./src/main/webapp/vue-app/news-details/main.js',
     latestNews:'./src/main/webapp/vue-app/latest-news/main.js',
     news :'./src/main/webapp/vue-app/news/main.js',
     newsSearchCard: './src/main/webapp/vue-app/news-search/main.js',
     newsDetailsApp: './src/main/webapp/vue-app/news-details-app/main.js',
-    scheduleNewsDrawer: './src/main/webapp/vue-app/schedule-news-drawer/main.js',
     newsListView: './src/main/webapp/vue-app/news-list-view/main.js',
     newsPublishTargetsManagement: './src/main/webapp/vue-app/news-publish-targets-management/main.js',
     newsActivityStreamExtensions: './src/main/webapp/vue-app/news-extensions/activity-stream-extensions/main.js',
