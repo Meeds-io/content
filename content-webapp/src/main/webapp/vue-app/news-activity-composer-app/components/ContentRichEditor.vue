1ecd89440090edd9fc48c0d3380fe64f91bcd9a8
Jihed Chabbeh <31882754+Jihed525@users.noreply.github.com>
2024-12-24 11:12:38 +0100

    fix: Editing a draft of a published article removes the article from published articles list - EXO-75992 - Meeds-io/meeds#2694
    Before this change, after creating a draft the published data of the
    targets will be false for news articles. To resolve this problem, when
    creating the draft, set the published value of this draft to the
    published value of the article if it already exists. After this change,
    the article is published under the adequate target.

--- content-webapp/src/main/webapp/vue-app/news-activity-composer-app/components/ContentRichEditor.vue
+++ content-webapp/src/main/webapp/vue-app/news-activity-composer-app/components/ContentRichEditor.vue
@@ -393,7 +393,7 @@ export default {
         title: this.article.title,
         body: this.replaceImagesURLs(this.$noteUtils.getContentToSave(this.editorBodyInputRef, this.oembedMinWidth)),
         published: this.originalArticle.published,
-        activityPosted: this.originalArticle.activityPosted,
+        activityPosted: this.article.activityPosted,
         schedulePostDate: this.article.schedulePostDate,
         publicationState: this.article.publicationState,
         scheduleUnpublishDate: this.article.scheduleUnpublishDate,
