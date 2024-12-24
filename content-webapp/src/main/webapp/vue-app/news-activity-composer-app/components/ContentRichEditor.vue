61eac1ac94a11a2381244c43022793893a1fe4fd
jihed525 <jchabbeh@exoplatform.com>
2024-12-24 11:08:18 +0100

    fix: Editing a draft of a published article removes the article from published articles list - EXO-75992 - Meeds-io/meeds#2694

--- content-webapp/src/main/webapp/vue-app/news-activity-composer-app/components/ContentRichEditor.vue
+++ content-webapp/src/main/webapp/vue-app/news-activity-composer-app/components/ContentRichEditor.vue
@@ -326,13 +326,13 @@ export default {
         this.article.targetPageId = createdArticle.targetPageId;
         this.article.properties = createdArticle.properties;
         this.article.draftPage = true;
-        this.article.body = createdArticle.body;
         this.article.published = createdArticle.published;
         this.article.targetPageId = createdArticle.targetPageId;
         this.article.publicationState = createdArticle.publicationState;
         this.article.schedulePostDate = createdArticle.schedulePostDate;
         this.article.publicationState = createdArticle.publicationState;
         this.article.scheduleUnpublishDate = createdArticle.scheduleUnpublishDate;
+        this.article.body = createdArticle.body;
         this.article.lang = createdArticle.lang;
         if (this.article.body !== createdArticle.body) {
           this.imagesURLs = this.extractImagesURLsDiffs(this.article.body, createdArticle.body);
