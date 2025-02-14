2693419332f6a4cbde94b4384bb00177f84ef45e
Ayoub Zayati <azayati@exoplatform.com>
2025-02-14 10:51:29 +0100

    fix: Replace url occurences of "news/editor" navigation node by "news-editor" navigation node - MEED-8425 - Meeds-io/meeds#2944 (#387)
    Prior to this change, "news/news-editor" navigation node of global site
    was not accessible to guests even if they are in related page access
    permissions since the parent navigation node "news" is not accessible to
    them. In order to allow guest to access to news editor, we have replaced
    the url occurences of "news/editor" navigation node by newly created
    navigation node "news-editor".
    
    Resolves https://github.com/Meeds-io/meeds/issues/2944

--- content-webapp/src/main/webapp/vue-app/news-extensions/activity-stream-extensions/components/activity/ActivityWriteNewsComposer.vue
+++ content-webapp/src/main/webapp/vue-app/news-extensions/activity-stream-extensions/components/activity/ActivityWriteNewsComposer.vue
@@ -90,7 +90,7 @@ export default {
   methods: {
     switchToNews() {
       if (!this.disableComposerButton) {
-        let url = `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/news/editor`;
+        let url = `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/news-editor`;
         if (eXo.env.portal.spaceId) {
           url += `?spaceId=${eXo.env.portal.spaceId}&spaceName=${eXo.env.portal.spaceName}&type=draft`;
         }
