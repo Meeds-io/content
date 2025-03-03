d829b76bd4b115bd3ffed89147234f419f05a698
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-03-03 12:08:48 +0100

    fix: Replace the delete option with the hide article activity option - MEED-8487 - Meeds-io/meeds#2872 (#410)
    Prior to this change, deleting an article activity from the activity
    stream did not update the article activity post status in the
    publication drawer. This change replaces the delete article activity
    option with the hide option and ensures that the correct value is set
    for the activity post property.

--- content-service/src/main/java/io/meeds/news/service/impl/NewsServiceImpl.java
+++ content-service/src/main/java/io/meeds/news/service/impl/NewsServiceImpl.java
@@ -1451,7 +1451,7 @@ public class NewsServiceImpl implements NewsService {
         article.setViewsCount(Long.parseLong(properties.get(NEWS_VIEWS)));
       }
       if (properties.containsKey(NEWS_ACTIVITY_POSTED)) {
-        article.setActivityPosted(Boolean.parseBoolean(properties.get(NEWS_ACTIVITY_POSTED)));
+        article.setActivityPosted(Boolean.parseBoolean(properties.get(NEWS_ACTIVITY_POSTED)) && !activityManager.getActivity(article.getActivityId()).isHidden());
       } else {
         article.setActivityPosted(false);
       }
