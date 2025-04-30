f374bf81c4df5cf45cc3aba1b452992c762acd4d
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-04-30 11:28:44 +0100

    feat: Move Links and Image Portlets From Social - Meeds-io/MIPs#191 (#499)
    This change will move Image and Links portlets from Social to centralize the CMS features inside content addon

---
+++ content-service/src/main/java/io/meeds/content/news/listener/AnalyticsNewsListener.java
@@ -0,0 +1,152 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.content.news.listener;
+
+import static io.meeds.analytics.utils.AnalyticsUtils.addSpaceStatistics;
+import static io.meeds.content.news.utils.NewsUtils.ADD_ARTICLE_TRANSLATION;
+import static io.meeds.content.news.utils.NewsUtils.COMMENT_NEWS;
+import static io.meeds.content.news.utils.NewsUtils.DELETE_NEWS;
+import static io.meeds.content.news.utils.NewsUtils.LIKE_NEWS;
+import static io.meeds.content.news.utils.NewsUtils.POST_NEWS;
+import static io.meeds.content.news.utils.NewsUtils.REMOVE_ARTICLE_TRANSLATION;
+import static io.meeds.content.news.utils.NewsUtils.SHARE_NEWS;
+import static io.meeds.content.news.utils.NewsUtils.UPDATE_NEWS;
+import static io.meeds.content.news.utils.NewsUtils.VIEW_NEWS;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.services.listener.Asynchronous;
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.listener.Listener;
+import org.exoplatform.services.listener.ListenerService;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
+
+import io.meeds.analytics.model.StatisticData;
+import io.meeds.analytics.utils.AnalyticsUtils;
+import io.meeds.content.news.model.News;
+import io.meeds.content.news.utils.NewsUtils;
+
+import jakarta.annotation.PostConstruct;
+
+@Asynchronous
+@Component
+@Profile("analytics")
+public class AnalyticsNewsListener extends Listener<String, News> {
+
+  private static final String   CREATE_CONTENT_OPERATION_NAME  = "createContent";
+
+  private static final String   UPDATE_CONTENT_OPERATION_NAME  = "updateContent";
+
+  private static final String   DELETE_CONTENT_OPERATION_NAME  = "deleteContent";
+
+  private static final String   VIEW_CONTENT_OPERATION_NAME    = "viewContent";
+
+  private static final String   SHARE_CONTENT_OPERATION_NAME   = "shareContent";
+
+  private static final String   LIKE_CONTENT_OPERATION_NAME    = "likeContent";
+
+  private static final String   COMMENT_CONTENT_OPERATION_NAME = "commentContent";
+
+  private static final String[] LISTENER_EVENTS                = { POST_NEWS, UPDATE_NEWS, DELETE_NEWS, VIEW_NEWS, SHARE_NEWS,
+      COMMENT_NEWS, LIKE_NEWS, ADD_ARTICLE_TRANSLATION, REMOVE_ARTICLE_TRANSLATION };
+
+  @Autowired
+  private IdentityManager       identityManager;
+
+  @Autowired
+  private SpaceService          spaceService;
+
+  @Autowired
+  private ListenerService       listenerService;
+
+  @PostConstruct
+  public void init() {
+    for (String listener : LISTENER_EVENTS) {
+      listenerService.addListener(listener, this);
+    }
+  }
+
+  @Override
+  public void onEvent(Event<String, News> event) throws Exception {
+    News news = event.getData();
+    String updater = event.getSource();
+    String operation = mapEventNameToOperation(event.getEventName());
+    long userId = 0;
+    Identity identity = getIdentityManager().getOrCreateUserIdentity(event.getSource());
+    if (identity != null) {
+      userId = Long.parseLong(identity.getId());
+    }
+    StatisticData statisticData = new StatisticData();
+
+    statisticData.setModule("contents");
+    statisticData.setSubModule("contents");
+    statisticData.setOperation(operation);
+    statisticData.setUserId(userId);
+    statisticData.addParameter("contentId", news.getId());
+    statisticData.addParameter("contentTitle", news.getTitle());
+    if (operation.equals(VIEW_CONTENT_OPERATION_NAME) || operation.equals(UPDATE_CONTENT_OPERATION_NAME)
+        || operation.equals(DELETE_CONTENT_OPERATION_NAME)) {
+      statisticData.addParameter("contentLanguage", news.getLang() != null ? news.getLang() : "originalVersion");
+    }
+    statisticData.addParameter("contentCreator", news.getOwner());
+    statisticData.addParameter("contentLastModifier", updater);
+    statisticData.addParameter("contentType", "News");
+    statisticData.addParameter("contentUpdatedDate", news.getUpdateDate());
+    statisticData.addParameter("contentCreationDate", news.getCreationDate());
+    Space space = getSpaceService().getSpaceById(news.getSpaceId());
+    if (space != null) {
+      addSpaceStatistics(statisticData, space);
+    }
+    AnalyticsUtils.addStatisticData(statisticData);
+  }
+
+  private String mapEventNameToOperation(String eventName) {
+    return switch (eventName) {
+    case "exo.news.postArticle" -> CREATE_CONTENT_OPERATION_NAME;
+    case "exo.news.updateArticle", NewsUtils.ADD_ARTICLE_TRANSLATION -> UPDATE_CONTENT_OPERATION_NAME;
+    case "exo.news.deleteArticle", NewsUtils.REMOVE_ARTICLE_TRANSLATION -> DELETE_CONTENT_OPERATION_NAME;
+    case "exo.news.viewArticle" -> VIEW_CONTENT_OPERATION_NAME;
+    case "exo.news.shareArticle" -> SHARE_CONTENT_OPERATION_NAME;
+    case "exo.news.commentArticle" -> COMMENT_CONTENT_OPERATION_NAME;
+    case "exo.news.likeArticle" -> LIKE_CONTENT_OPERATION_NAME;
+    default -> throw new IllegalArgumentException("Unknown event: " + eventName);
+    };
+  }
+
+  public IdentityManager getIdentityManager() {
+    if (identityManager == null) {
+      identityManager = ExoContainerContext.getService(IdentityManager.class);
+    }
+    return identityManager;
+  }
+
+  public SpaceService getSpaceService() {
+    if (spaceService == null) {
+      spaceService = ExoContainerContext.getService(SpaceService.class);
+    }
+    return spaceService;
+  }
+}
---
+++ content-service/src/main/java/io/meeds/content/news/plugin/ArticlePageAttachmentPlugin.java
@@ -0,0 +1,84 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.content.news.plugin;
+
+import static io.meeds.content.news.utils.NewsUtils.NewsObjectType.ARTICLE;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import org.exoplatform.commons.exception.ObjectNotFoundException;
+import org.exoplatform.services.security.Identity;
+import org.exoplatform.social.attachment.AttachmentPlugin;
+import org.exoplatform.social.attachment.AttachmentService;
+
+import io.meeds.content.news.model.News;
+import io.meeds.content.news.service.NewsService;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class ArticlePageAttachmentPlugin extends AttachmentPlugin {
+
+  @Autowired
+  private AttachmentService  attachmentService;
+
+  @Autowired
+  private NewsService        newsService;
+
+  public static final String OBJECT_TYPE = "articlePage";
+
+  @PostConstruct
+  public void init() {
+    attachmentService.addPlugin(this);
+  }
+
+  @Override
+  public String getObjectType() {
+    return OBJECT_TYPE;
+  }
+
+  @Override
+  public boolean hasAccessPermission(Identity identity, String articleId) throws ObjectNotFoundException {
+    News news = newsService.getNewsArticleById(articleId);
+    return news != null && newsService.canViewNews(news, identity.getUserId());
+  }
+
+  @Override
+  public boolean hasEditPermission(Identity identity, String articleId) throws ObjectNotFoundException {
+    News news = null;
+    try {
+      news = newsService.getNewsById(articleId, identity, false, ARTICLE.name());
+    } catch (IllegalAccessException e) {
+      return false;
+    }
+    return news != null && news.isCanEdit();
+  }
+
+  @Override
+  public long getAudienceId(String s) throws ObjectNotFoundException {
+    return 0;
+  }
+
+  @Override
+  public long getSpaceId(String articleId) throws ObjectNotFoundException {
+    News news = newsService.getNewsArticleById(articleId);
+    return news != null ? Long.parseLong(news.getSpaceId()) : 0;
+  }
+}
