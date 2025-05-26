48365385d93f85fc5d9c915bfde95079db1b80b1
Anwar khanfir <akhanfir@exoplatform.com>
2025-05-26 12:02:33 +0100

    fix: Set an empty alt to images of published and pinned article and to the avatar of whom pinned it - MEED-9171 - Meeds-io/meeds#3345. Before this change, when create an article and access to the stream page, a class a containing a title=Click here for more details. To fix this problem, delete this title. After this change, no tooltip is displayed in the activity in the component which opens the activity detail. Resolves https://github.com/Meeds-io/meeds/issues/3345

--- content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsSliderView.vue
+++ content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsSliderView.vue
@@ -39,8 +39,7 @@
               ? item.illustrationURL.concat('&size=1420x222')
               : '/content/images/news.png'"
             alt=""
-            class="articleImage object-fit-cover width-full full-height"
-          />
+            class="articleImage object-fit-cover width-full full-height">
           <v-container class="slide-text-container d-flex text-center">
             <div class="flex flex-column carouselNewsInfo">
               <div
