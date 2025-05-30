36e03e627a53a6b89e6d9b2b180984dcd49825b1
Anwar khanfir <93767376+akhanfir@users.noreply.github.com>
2025-05-30 10:16:18 +0100

    fix: Set a suitable alt to images of published article - MEED-9198 - Meeds-io/meeds#3363 (#526)
    Before this change, when create an article with image and publish it in
    a News list, The image of the published article in the News list has alt
    empty. To fix this problem, add the alt text of news properties
    otherwise keep the alt empty. After this change, the alt text of the
    image is displayed correctly.
    
    (cherry picked from commit fb9b3f02a1f28ad4b21f1f50e7b97f176c2f33f0)

--- content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsSliderView.vue
+++ content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsSliderView.vue
@@ -38,7 +38,7 @@
             :src="showArticleImage && item.illustrationURL !== null
               ? item.illustrationURL.concat('&size=1420x222')
               : '/content/images/news.png'"
-            alt=""
+            :alt="item?.properties?.featuredImage?.altText || ''"
             class="articleImage object-fit-cover width-full full-height">
           <v-container class="slide-text-container d-flex text-center">
             <div class="flex flex-column carouselNewsInfo">
