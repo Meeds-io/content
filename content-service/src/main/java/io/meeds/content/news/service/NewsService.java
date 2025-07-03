f9c7d0273bdfcf47a62b9299789483eab3ff721a
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-07-03 11:24:56 +0100

    feat: Improve retrieval of space identity when searching news of a space - MEED-9337 - Meeds-io/MIPs#200 (#551)

--- content-service/src/main/java/io/meeds/content/news/service/NewsService.java
+++ content-service/src/main/java/io/meeds/content/news/service/NewsService.java
@@ -52,6 +52,7 @@ import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.LocaleUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.exoplatform.portal.application.localization.LocalizationFilter;
+import org.exoplatform.social.core.space.SpaceUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -964,18 +965,8 @@ public class NewsService {
    */
   public List<NewsSearchResultEntity> search(org.exoplatform.social.core.identity.model.Identity currentIdentity,
                                              NewsFilter filter) {
-    List<String> spaceIds = filter.getSpaces();
-    if (!CollectionUtils.isEmpty(spaceIds)) {
-      List<String> identityIds = spaceIds.stream()
-              .map(spaceService::getSpaceById)
-              .filter(Objects::nonNull)
-              .map(Space::getPrettyName)
-              .map(identityManager::getOrCreateSpaceIdentity)
-              .filter(Objects::nonNull)
-              .map(identity -> Long.toString(identity.getIdentityId()))
-              .toList();
-
-      filter.setSpaces(identityIds);
+    if (CollectionUtils.isNotEmpty(filter.getSpaces())) {
+      filter.setSpaces(SpaceUtils.getSpaceIdentityIds(currentIdentity.getRemoteId(),  filter.getSpaces()));
     }
     List<NewsESSearchResult> searchResults = newsSearchConnector.search(currentIdentity, filter);
     return searchResults.stream().map(result -> {
