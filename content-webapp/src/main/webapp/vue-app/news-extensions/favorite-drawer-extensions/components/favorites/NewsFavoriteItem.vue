130c8c61680f6bf6938cc0d307886c6e6b36e1e2
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-07-11 13:24:19 +0100

    feat: Improve Favorite News Display when drawer expanded - MEED-9435 - Meeds-io/MIPs#209 (#558)

--- content-webapp/src/main/webapp/vue-app/news-extensions/favorite-drawer-extensions/components/favorites/NewsFavoriteItem.vue
+++ content-webapp/src/main/webapp/vue-app/news-extensions/favorite-drawer-extensions/components/favorites/NewsFavoriteItem.vue
@@ -25,24 +25,35 @@
     @auxclick="setAsViewed"
     @click="setAsViewed">
     <v-list-item-icon class="me-3 my-auto">
-      <v-avatar
-        tile
-        size="25">
-        <img
-          v-if="news?.illustrationURL"
-          :alt="featuredImageAltText"
-          :src="`${news.illustrationURL}&size=25x25`">
-        <img
-          v-else
-          :alt="activityTitle"
-          src="/content/images/news.png">
-      </v-avatar>
+      <v-card
+        :min-width="iconWidth"
+        class="d-flex justify-center no-border-radius"
+        color="transparent"
+        flat>
+        <v-icon :size="iconSize">fa-newspaper</v-icon>
+      </v-card>
     </v-list-item-icon>
-
     <v-list-item-content>
-      <v-list-item-title class="text-color">{{ activityTitle }}</v-list-item-title>
+      <v-list-item-title class="text-color">{{ title }}</v-list-item-title>
+      <v-list-item-subtitle v-if="expanded" class="d-flex align-center full-width overflow-hidden pt-2px">
+        <template v-if="spaceId">
+          <space-avatar
+            :space-id="spaceId"
+            :size="16"
+            class="flex-grow-0 flex-shrink-1 text-truncate"
+            link-style />
+          <v-icon class="flex-grow-0 flex-shrink-0 mx-2" size="2">fa-circle</v-icon>
+        </template>
+        <date-format class="flex-grow-0 flex-shrink-0" :value="date" />
+        <template v-if="updater">
+          <v-icon class="flex-grow-0 flex-shrink-0 mx-2" size="2">fa-circle</v-icon>
+          <user-avatar
+            :profile-id="updater"
+            :size="16"
+            class="flex-grow-0 flex-shrink-1 text-truncate" />
+        </template>
+      </v-list-item-subtitle>
     </v-list-item-content>
-
     <v-list-item-action>
       <favorite-button
         :id="id"
@@ -74,15 +85,31 @@ export default {
   },
   data: () => ({
     news: null,
-    activityTitle: '',
-    url: '',
     isFavorite: true,
     newsObjectType: 'article',
   }),
   computed: {
-    featuredImageAltText() {
-      return this.news?.properties?.featuredImage?.altText || this.activityTitle;
-    }
+    iconWidth() {
+      return this.expanded ? 40 : 30;
+    },
+    iconSize() {
+      return this.expanded ? 34 : 24;
+    },
+    title() {
+      return this.news?.title ? this.$utils.htmlToText(this.news.title) : '';
+    },
+    url() {
+      return this.news?.url;
+    },
+    spaceId() {
+      return this.news?.spaceId;
+    },
+    updater() {
+      return this.news ? this.news?.updater || this.news?.author : null;
+    },
+    date() {
+      return this.news?.updateDate || this.news?.publicationDate || this.news?.creationDate;
+    },
   },
   created() {
     let newsId = this.id;
@@ -93,11 +120,7 @@ export default {
       lang = parts[1];
     }
     this.$newsServices.getNewsById(newsId, false, this.newsObjectType, lang)
-      .then(news => {
-        this.activityTitle = news.title;
-        this.url = news.url;
-        this.news = news;
-      });
+      .then(news => this.news = news);
   },
   methods: {
     removed() {
