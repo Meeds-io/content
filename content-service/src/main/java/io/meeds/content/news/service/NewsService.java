bb63e90328ff15877c953ca0ca8c832df036314e
Anwar khanfir <93767376+akhanfir@users.noreply.github.com>
2025-07-16 10:02:03 +0100

    fix: null value retreived when searching news prevents from displaying results - MEED-9395 - meeds-io/meeds#3468 (#561)
    before this change, when create 3 news which contains a common word then delete the metadata of one of these news with a query and go to news app in filter field type of common word, no articles found for this search. To fix this problem, filter the return list to remove news that is null. After this change, the retrieved news is displayed.

--- content-service/src/main/java/io/meeds/content/news/service/NewsService.java
+++ content-service/src/main/java/io/meeds/content/news/service/NewsService.java
@@ -808,7 +808,7 @@ public class NewsService {
         LOG.error("Error while building news article", e);
         return null;
       }
-    }).toList();
+    }).filter(Objects::nonNull).toList();
   }
 
   /**
