6e28860afcabef840044d964979343c1d61e1991
Ayoub Zayati <azayati@exoplatform.com>
2025-08-14 10:40:22 +0100

    feat: Separate author and publisher in the different display of an article/note - MEED-9410 - Meeds-io/meeds#3462 (#590)

--- content-service/src/main/java/io/meeds/content/news/service/NewsService.java
+++ content-service/src/main/java/io/meeds/content/news/service/NewsService.java
@@ -1192,6 +1192,7 @@ public class NewsService {
     Space space = spaceService.getSpaceById(article.getSpaceId());
     if (articlePage != null && space != null) {
       PageVersion pageVersion = noteService.getPublishedVersionByPageIdAndLang(Long.parseLong(articlePage.getId()), null);
+      article.setAuthor(pageVersion != null ? pageVersion.getAuthor() : articlePage.getAuthor());
       article.setIllustrationURL(NewsUtils.buildIllustrationUrl(articlePage.getProperties(), articlePage.getLang()));
       buildNewArticleProperties(article, articlePage, creator, space.getId(), pageVersion.getId(), true);
     }
