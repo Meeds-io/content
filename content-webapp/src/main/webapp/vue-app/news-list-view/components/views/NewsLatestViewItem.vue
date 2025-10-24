03794df0f9c845eab8e640bc462fe2bb37dc9a40
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2025-10-24 12:32:39 +0100

    fix: Add a hidden span text in newsList reactions -MEED-9861 (#616)
    This change will add a hidden span text in newsList reactions (likes, comments and views counter).

--- content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsLatestViewItem.vue
+++ content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsLatestViewItem.vue
@@ -54,14 +54,17 @@
             <v-icon class="reactionIconStyle me-1" size="12">
               mdi-thumb-up
             </v-icon>
+            <span class="screen-reader-only">{{ $t('news.app.number.likes') }}</span>  
             <div class="likesCount me-2">{{ item.likesCount }}</div>
             <v-icon class="reactionIconStyle commentStyle me-1" size="12">
               mdi-comment
             </v-icon>
+            <span class="screen-reader-only">{{ $t('news.app.number.comments') }}</span>  
             <div class="commentsCount me-2">{{ item.commentsCount }}</div>
             <v-icon class="reactionIconStyle me-1" size="12">
               mdi-eye
             </v-icon>
+            <span class="screen-reader-only">{{ $t('news.app.number.views') }}</span>  
             <div class="viewCount">{{ item.viewsCount }}</div>
           </div>
         </div>
