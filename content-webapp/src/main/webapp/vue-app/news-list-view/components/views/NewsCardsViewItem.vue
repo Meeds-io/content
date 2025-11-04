0af8639c056184139eea2559401538af884669c1
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-11-04 13:26:35 +0100

    fix: Change tabulation behavior on articles displayed in cards template - MEED-9594 - Meeds-io/meeds#3589
    Before this change, when navigating using tab in the list of articles we can focus on each details of an article.
     After this commit, the tab navigation is changed to focus on the whole article and tab press moved to the next article, also pressing enter opens the selected article in the same tab.

--- content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsCardsViewItem.vue
+++ content-webapp/src/main/webapp/vue-app/news-list-view/components/views/NewsCardsViewItem.vue
@@ -41,7 +41,6 @@
         class="upper-row">
         <div
           v-if="!isHiddenSpace && showArticleSpace"
-          tabindex="-1"
           :id="`space-link-${item.activityId}`"
           class="space-link"
           :aria-label="$t('news.space.icon.title', { 0: item.spaceDisplayName })">
