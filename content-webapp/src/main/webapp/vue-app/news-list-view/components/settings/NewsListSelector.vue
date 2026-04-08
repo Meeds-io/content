32ccd569ff7e338c8cafb3b1c1cc69c89cfa00fb
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2026-04-08 09:22:23 +0100

    feat: add accessibility requirements - 10352 - Meeds-io/MIPs#247 (#707)

--- content-webapp/src/main/webapp/vue-app/news-list-view/components/settings/NewsListSelector.vue
+++ content-webapp/src/main/webapp/vue-app/news-list-view/components/settings/NewsListSelector.vue
@@ -70,7 +70,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
             v-if="index !== 0"
             icon
             small
-            :title="$t('news.list.settings.source.moveUp.title')"
+            :title="$t('news.list.settings.source.moveUp.title', {0: article?.title})"
             @click="moveUp(index)">
             <v-icon small>fa-arrow-up</v-icon>
           </v-btn>
@@ -78,7 +78,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
             v-if="index !== lastSelectedIndex"
             icon
             small
-            :title="$t('news.list.settings.source.moveDown.title')"
+            :title="$t('news.list.settings.source.moveDown.title', {0: article?.title})"
             @click="moveDown(index)">
             <v-icon small>fa-arrow-down</v-icon>
           </v-btn>
@@ -86,7 +86,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
             icon
             small
             class="ml-2"
-            :title="$t('news.list.settings.source.remove.title')"
+            :title="$t('news.list.settings.source.remove.title', {0: article?.title})"
             @click="remove(article)">
             <v-icon class="error-color" small>
               fa-trash
