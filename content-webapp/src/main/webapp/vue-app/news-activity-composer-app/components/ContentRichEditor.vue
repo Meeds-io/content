931511a7f19c2c47a153db740ff5590e3108de0e
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2026-06-24 15:07:03 +0100

    fix: Fix news draft after adding/ deleting images - EXO-87150 (#801)
    Prior to this change, deleting an image and then editing the draft body could cause remaining images to lose their correct src. This change will make image URL handling more robust by improving how mappings are
     racked and applied, ensuring that images keep their proper source even after deletions or body edits.

--- content-webapp/src/main/webapp/vue-app/news-activity-composer-app/components/ContentRichEditor.vue
+++ content-webapp/src/main/webapp/vue-app/news-activity-composer-app/components/ContentRichEditor.vue
@@ -655,6 +655,10 @@ export default {
 
       originalImages.each(function(index, element) {
         const originalImageURL = $(element).attr('src');
+        const updatedElement = updatedImages[index];
+        if (!updatedElement) {
+          return;
+        }
         const updatedImageURL = $(updatedImages[index]).attr('src');
         if (updatedImageURL !== originalImageURL) {
           imagesURLs.set(originalImageURL, updatedImageURL);
