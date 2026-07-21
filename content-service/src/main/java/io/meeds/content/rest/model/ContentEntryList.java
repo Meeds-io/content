f2e9003bb42c8a5eec5b9e177686e8e1094ff911
Azmi Touil <atouil@exoplatform.com>
2026-07-21 18:54:45 +0200

    feat: merge News and Notes into a real Content List app - EXO-88569 - eXIP7.3.0.12

--- content-service/src/main/java/io/meeds/content/rest/model/ContentEntryList.java
+++ content-service/src/main/java/io/meeds/content/rest/model/ContentEntryList.java
@@ -7,7 +7,6 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -17,14 +16,24 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-import ContentListView from './components/ContentListView.vue';
-import ContentListItem from './components/ContentListItem.vue';
+package io.meeds.content.rest.model;
 
-const components = {
-  'content-list-view': ContentListView,
-  'content-list-item': ContentListItem,
-};
+import java.util.List;
 
-for (const key in components) {
-  Vue.component(key, components[key]);
+import io.meeds.content.model.ContentEntry;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ContentEntryList {
+
+  private List<ContentEntry> items;
+
+  private int                 offset;
+
+  private int                 limit;
+
+  private int                 size;
+
 }
