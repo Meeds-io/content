a2f2340cc45e577d540b65fa915865a3aa9db3cf
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2026-08-31 15:37:15 +0100

    fix: Make News List Header using Layout Style - EXO-89148 (#903)
    This change will allow to use Layout Text Header style in News List
    Application.

--- content-webapp/src/main/webapp/vue-app/news-list-view/components/settings/NewsSettings.vue
+++ content-webapp/src/main/webapp/vue-app/news-list-view/components/settings/NewsSettings.vue
@@ -26,8 +26,10 @@
     <div class="d-flex latestNewsTitleContainer flex-column flex-grow-1 min-width-0 my-1">
       <span
         v-if="showHeader"
-        class="text-header text-truncate d-block line-height-normal"
-        :title="headerTitle">{{ headerTitle }}</span>
+        :title="headerTitle"
+        class="text-header">
+        <span class="text-truncate">{{ headerTitle }}</span>
+      </span>
     </div>
     <div v-if="showSeeAll && !hideSeeAllButton" class="d-flex flex-column my-auto me-2">
       <v-btn
